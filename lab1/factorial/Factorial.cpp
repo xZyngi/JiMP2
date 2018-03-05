94d9d9e9b47e54420f62c31cb8634c951de5938b
xZyngi <xZyngi@github.com>
2018-03-05 22:42:17 +0100

    Fixed Factorial

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,9 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+  if(value < -12 || value > 12) return 0;
+
+  if(value < 0) return value*factorial(value+1);
+  if(value < 2) return 1;
+  return value*factorial(value-1);
 }
