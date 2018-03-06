6c545296a14dfae4c4eeca398e08d10229139412
xZyngi <xZyngi@github.com>
2018-03-06 11:30:46 +0100

    Add Palindrome (lab1)

---
+++ lab1/palindrome/Palindrome.h
@@ -0,0 +1,16 @@
+//
+// Created by zyngi on 06.03.18.
+//
+
+#ifndef JIMP_EXERCISES_PALINDROME_H
+#define JIMP_EXERCISES_PALINDROME_H
+
+#include <iostream>
+#include <string>
+#include "Palindrome.h"
+
+using namespace std;
+
+bool is_palindrome(string str);
+
+#endif //JIMP_EXERCISES_PALINDROME_H
