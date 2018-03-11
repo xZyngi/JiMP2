c0952a626ad2c1eea7a12511a4e514f4d88852d1
xZyngi <xZyngi@github.com>
2018-03-11 18:48:02 +0100

    update polybius (lab2)

--- lab2/polybius/main.cpp
+++ lab2/polybius/main.cpp
@@ -3,15 +3,49 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include "Polybius.h"
 
 using namespace std;
 
 int main(){
 
-    cout << PolybiusCrypt("message");
-    cout << endl;
-    cout << PolybiusDecrypt("32154343112215");
+    int task;
+    string input_file, output_file, result;
+
+    cin >> input_file;
+    cin >> output_file;
+    cin >> task;
+
+    ifstream my_file(input_file);
+    string file_content;
+
+    if(!my_file) {
+        return 0;
+    }
+
+    while(my_file >> file_content){
+        //READING FILE
+    }
+
+
+    if(task == 1) {
+        result = PolybiusCrypt("message");
+
+    }
+    else if(task == 0) {
+        result = PolybiusDecrypt("32154343112215");
+    }
+    else{
+        return 0;
+    }
+
+    cout << result << endl;
+
+
+    std::ofstream out(output_file);
+    out << result;
+    out.close();
 
     return 0;
 }
