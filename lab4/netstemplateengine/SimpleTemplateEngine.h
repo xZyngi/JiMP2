f355ad719dbea86f952ff5063c60dca43c114fff
xZyngi <xZyngi@github.com>
2018-04-01 15:58:02 +0200

    update SimpleTemplateEngine (without injection)

--- lab4/netstemplateengine/SimpleTemplateEngine.h
+++ lab4/netstemplateengine/SimpleTemplateEngine.h
@@ -5,10 +5,30 @@
 #ifndef JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
 #define JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
 
+#include <iostream>
+#include <string>
+#include <unordered_map>
 
-class SimpleTemplateEngine {
+using namespace std;
 
-};
+namespace nets {
+    class View {
+    public:
+        View();
 
+        View(std::string text);
+
+        ~View();
+
+        string Render(const std::unordered_map<std::string, std::string> &model) const;
+
+        string GetText() const;
+
+        void SetText(std::string text);
+
+    private:
+        string text;
+    };
+}
 
 #endif //JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
