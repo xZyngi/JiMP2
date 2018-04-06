8fa1e9a00c37d785080a21e4eddbd0936919af75
xZyngi <zyngi98@gmail.com>
2018-04-06 10:37:37 +0200

    update SimpleJson

--- lab4/netsjson/SimpleJson.cpp
+++ lab4/netsjson/SimpleJson.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "SimpleJson.h"
+
+namespace nets {
+    nets::JsonValue::JsonValue(map<string, nets::JsonValue> value) {
+        m_json = value;
+    }
+
+    nets::JsonValue::JsonValue(int value) {
+        i_json = value;
+    }
+
+    nets::JsonValue::JsonValue(double value) {
+        d_json = value;
+    }
+
+    nets::JsonValue::JsonValue(bool value) {
+        b_json = value;
+    }
+
+    nets::JsonValue::JsonValue(string value) {
+        s_json = value;
+    }
+
+    nets::JsonValue::JsonValue(vector<nets::JsonValue> value) {
+        v_json = value;
+    }
+
+    nets::JsonValue::~JsonValue() {
+
+    }
+
+    nets::JsonValue nets::JsonValue::ValueByName(const string &name) const {
+        for (auto n : m_json) {
+            if (n.first == name) {
+                return n.second;
+            }
+        }
+    }
+
+    string nets::JsonValue::ToString() const {
+        return std::__cxx11::string();
+    }
+}
--- lab4/netsjson/SimpleJson.h
+++ lab4/netsjson/SimpleJson.h
@@ -5,10 +5,36 @@
 #ifndef JIMP_EXERCISES_SIMPLEJSON_H
 #define JIMP_EXERCISES_SIMPLEJSON_H
 
+#include <experimental/optional>
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
 
-class SimpleJson {
+using namespace std;
 
-};
+namespace nets {
+    class JsonValue {
+    public:
+        JsonValue(map<string, JsonValue> value);
+        JsonValue(int value);
+        JsonValue(double value);
+        JsonValue(bool value);
+        JsonValue(string value);
+        JsonValue(vector<JsonValue> value);
+        ~JsonValue();
+
+        JsonValue ValueByName(const string &name) const;
+        string ToString() const;
+    private:
+        map<string, JsonValue> m_json;
+        experimental::optional<int> i_json;
+        experimental::optional<double> d_json;
+        experimental::optional<bool> b_json;
+        experimental::optional<string> s_json;
+        experimental::optional<vector<JsonValue>> v_json;
+    };
+}
 
 
 #endif //JIMP_EXERCISES_SIMPLEJSON_H
