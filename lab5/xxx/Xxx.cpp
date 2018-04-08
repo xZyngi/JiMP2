8cbfe3e5b8fb378b7a6a1cebafc8b316e1f0b514
zyngjaku <zyngjaku@github.com>
2018-04-08 12:54:48 +0200

    update xxx

--- lab5/xxx/Xxx.cpp
+++ lab5/xxx/Xxx.cpp
@@ -4,19 +4,22 @@
 
 #include "Xxx.h"
 
-XXX::XXX() {
+XXX::XXX() : name_{new char[1024]} {
 
 }
 
-XXX::XXX(int param) {
-
+XXX::~XXX() {
+    delete [] name_;
 }
 
-XXX::XXX(const std::string name) {
-
+XXX::XXX(const XXX& xxx) {
+    size_t sz = strlen(xxx.name_);
+    name_ = new char[sz];
+    strcpy(name_,xxx.name_);
+    //Teraz nowy obiekt pokazuje na nowy fragment pamięci,
+    //ale ze skopiowaną informacją
 }
 
-//konstruktor przenoszący:
 XXX::XXX(XXX &&xxx) : name_{nullptr} {
     swap(name_,xxx.name_);
     //Bardzo popularna szutczka
@@ -26,7 +29,22 @@ XXX::XXX(XXX &&xxx) : name_{nullptr} {
     //delete nullptr jest bezpieczna operacją i nic się nie stanie...
 }
 
-//operator przenoszący:
+//operator przypisania:
+XXX & XXX::operator=(const XXX& xxx) {
+    //jeśli ktoś wpadł na pomysł x = x;
+    if (this == &xxx) {
+        return *this;
+    }
+    //w przyciwynym wypadku mamy x = y;
+    //musimy sami zwolnic pamięć po x (czyli this):
+    delete[] name_;
+    //i wreszcie kopiowanie, ten kod jest
+    //jest identyczny więc można by go wydzielić do innej metody...
+    size_t sz = strlen(xxx.name_);
+    name_ = new char[sz];
+    strcpy(name_,xxx.name_);
+}
+
 XXX & XXX::operator=(XXX &&xxx) {
     //jeśli ktoś wpadł na pomsył x = move(x);
     if (this == &xxx) {
@@ -39,8 +57,4 @@ XXX & XXX::operator=(XXX &&xxx) {
     //jest identyczny więc można by go wydzielić do innej metody...
     name_ = nullptr;
     swap(name_,xxx.name_);
-}
-
-XXX::~XXX() {
-    delete [] name_;
 }
--- lab5/xxx/main.cpp
+++ lab5/xxx/main.cpp
@@ -2,13 +2,26 @@
 // Created by zyngjaku on 06.04.18.
 //
 
-#include <curses.h>
 #include "Xxx.h"
 
 int main(){
 
-    XXX old;
-    XXX new_xxx{old};
+    //Konstrukcja obiektu za pomocą domyślnego konstruktora
+    //nieciekawe
+    XXX old_xxx {};
+
+    //Konstrukcja obiektu na podstawie już zaincjalizowanego
+    XXX new_xxx {old_xxx};
+
+    //znowu nieciekawy konstruktor domyślny...
+    XXX another_xxx {};
+
+    //tutaj przypisujemy stan obiektu jednego do drugiego
+    //ale obydwa są już zaincjalizowane...
+    another_xxx = new_xxx;
+
+    //tutaj kończy się zakres funkcji main i wszystkie trzy obiekty tracą ważność
+    //zostają wywołane więc destruktory
 
     return 0;
 }
