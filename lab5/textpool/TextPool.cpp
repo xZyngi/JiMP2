58c98df86cba517421d8542eef14bb85dbaa7549
zyngjaku <zyngjaku@github.com>
2018-04-08 15:29:31 +0200

    add textpool template

--- lab5/textpool/TextPool.cpp
+++ lab5/textpool/TextPool.cpp
@@ -2,3 +2,23 @@
 // Created by zyngjaku on 06.04.18.
 //
 
+#include <experimental/string_view>
+#include "TextPool.h"
+
+namespace pool {
+    TextPool::TextPool() {
+
+    }
+
+    TextPool::TextPool(initializer_list<string> str) {
+
+    }
+
+    std::experimental::string_view TextPool::Intern(const std::string &str) {
+        return nullptr;
+    }
+
+    size_t TextPool::StoredStringCount() const {
+        return 0;
+    }
+}
--- lab5/textpool/TextPool.h
+++ lab5/textpool/TextPool.h
@@ -5,4 +5,23 @@
 #ifndef JIMP_EXERCISES_TEXTPOOL_H
 #define JIMP_EXERCISES_TEXTPOOL_H
 
+#include <iostream>
+#include <initializer_list>
+#include <string>
+#include <experimental/string_view>
+#include <set>
+
+using namespace std;
+
+namespace pool{
+    class TextPool{
+    public:
+        TextPool();
+        TextPool(initializer_list<string> str);
+
+        std::experimental::string_view Intern(const std::string &str);
+        size_t StoredStringCount() const;
+    };
+}
+
 #endif //JIMP_EXERCISES_TEXTPOOL_H
