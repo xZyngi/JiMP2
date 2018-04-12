7f8d935f0eddf4f847c5aacff1f4ef373013938b
zyngjaku <zyngjaku@github.com>
2018-04-12 12:32:10 +0200

    add matrix template

--- lab5/matrix/Matrix.cpp
+++ lab5/matrix/Matrix.cpp
@@ -2,3 +2,72 @@
 // Created by zyngjaku on 06.04.18.
 //
 
+#include "Matrix.h"
+
+Matrix::Matrix() {
+
+}
+
+Matrix::Matrix(int number_of_col, int number_of_row) {
+
+}
+
+Matrix::Matrix(const std::string &str) {
+
+}
+
+Matrix::Matrix(const std::initializer_list<std::vector<std::complex<double>>> &elements) {
+
+}
+
+Matrix::Matrix(const Matrix &matrix) {
+
+}
+
+Matrix &Matrix::operator=(const Matrix &matrix) {
+
+}
+
+std::pair<size_t, size_t> Matrix::size() const {
+    return pair<size_t, size_t>();
+}
+
+std::string Matrix::print() const {
+    return std::__cxx11::string();
+}
+
+std::complex<double> Matrix::pop(int cols, int rows) const {
+    return complex<double>();
+}
+
+void Matrix::Set(int cols, int rows, std::complex<double> val) {
+
+}
+
+Matrix Matrix::add(const Matrix &mat2) const {
+    return Matrix();
+}
+
+Matrix Matrix::sub(const Matrix &mat2) {
+    return Matrix();
+}
+
+Matrix Matrix::mul(Matrix mat2) {
+    return Matrix();
+}
+
+Matrix Matrix::mul(int x) {
+    return Matrix();
+}
+
+Matrix Matrix::div(Matrix mat2) {
+    return Matrix();
+}
+
+Matrix Matrix::div(int x) {
+    return Matrix();
+}
+
+Matrix Matrix::pow(int x) {
+    return Matrix();
+}
--- lab5/matrix/Matrix.h
+++ lab5/matrix/Matrix.h
@@ -5,4 +5,43 @@
 #ifndef JIMP_EXERCISES_MATRIX_H
 #define JIMP_EXERCISES_MATRIX_H
 
+#include <iostream>
+#include <vector>
+#include <complex>
+
+using namespace std;
+
+class Matrix {
+public:
+    Matrix();
+    Matrix (int number_of_col, int number_of_row);
+
+    Matrix (const std::string &str);
+    Matrix (const std::initializer_list<std::vector<std::complex<double>>> &elements);
+
+    Matrix(const Matrix &matrix);
+    Matrix &operator=(const Matrix &matrix);
+
+    std::pair<size_t , size_t > size() const;
+    std::string print() const;
+    std::complex<double> pop(int cols, int rows) const;
+
+    void Set(int cols, int rows, std::complex<double> val);
+
+    Matrix add(const Matrix &mat2) const;
+    Matrix sub(const Matrix &mat2);
+    Matrix mul(Matrix mat2);
+    Matrix mul(int x);
+    Matrix div( Matrix mat2);
+    Matrix div(int x);
+    Matrix pow(int x);
+
+private:
+    complex<double> matrix; //TODO: Make 2D dynamic array
+
+    int number_of_row_;
+    int number_od_col_;
+};
+
+
 #endif //JIMP_EXERCISES_MATRIX_H
