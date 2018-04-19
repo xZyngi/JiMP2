b541cef56c4717e4a13a42c0e07c4e95f6268719
Kamila <owsiankakamila@gmail.com>
2018-04-19 20:04:12 +0200

    WordCounter .cpp .h

--- lab6/wordcounter/WordCounter.cpp
+++ lab6/wordcounter/WordCounter.cpp
@@ -1,5 +1,104 @@
 //
-// Created by zyngi on 16.04.18.
+// Created by kamila on 14.04.18.
 //
 
 #include "WordCounter.h"
+
+datastructures::Counts datastructures::WordCounter::not_found =0;
+
+
+datastructures::WordCounter::WordCounter(const std::initializer_list<datastructures::Word> &elements) {
+
+    for (auto n: elements){
+        if(not index_.empty()){
+            auto search = index_.find(n);
+
+            if (search != index_.end()) {
+                ++search->second;
+            }
+            else{
+                index_.insert ( std::pair<Word,Counts>(n,1));
+            }
+        }
+
+
+        else{
+            index_.insert ( std::pair<Word,Counts>(n,1));
+        }
+    }
+
+
+}
+
+int datastructures::WordCounter::DistinctWords() const {
+    //rozmiar kontenera
+    return index_.size();
+}
+
+int datastructures::WordCounter::TotalWords() const {
+    //przejdz po kolei po kontenerze i zlicze Counts
+    int total=0;
+    for (auto n:index_){
+        total = total + n.second;
+    }
+    return total;
+}
+
+std::set<datastructures::Word> datastructures::WordCounter::Words() const {
+    std::set<Word> words;
+    for (auto n:index_){
+        words.insert(n.first);
+    }
+    return words;
+}
+
+datastructures::Counts &datastructures::WordCounter::operator[] (std::string word) {
+
+    auto search = index_.find(word);
+    // index_ to std::map <Word,Counter>
+
+
+    if (search != index_.end()) {
+        return search->second;
+    }
+    else{
+
+        return not_found;
+    }
+
+
+
+}
+
+
+std::ostream &datastructures::operator<<(std::ostream &output, const datastructures::WordCounter &counter) {
+    std::map <Word,Counts> index = counter.index_;
+    //sort
+
+    //<<
+
+    return output;
+}
+
+
+
+datastructures::WordCounter datastructures::WordCounter::FromInputStream(std::istream &text) {
+
+    // WordCounter wc = WordCounter::FromInputStream(&is);
+
+
+
+    // leave only ' ' and letters
+
+    // save word until ' '
+    // counts =counter [word] //reference is returned.. so i can change??"
+    //  if (counts == 0 )
+    //insert
+    // else{
+    //++counts //pointer??
+
+    return datastructures::WordCounter();
+}
+
+
+
--- lab6/wordcounter/WordCounter.h
+++ lab6/wordcounter/WordCounter.h
@@ -1,8 +1,105 @@
 //
-// Created by zyngi on 16.04.18.
+// Created by kamila on 14.04.18.
 //
 
 #ifndef JIMP_EXERCISES_WORDCOUNTER_H
 #define JIMP_EXERCISES_WORDCOUNTER_H
+
+#include <iostream>
+#include <map>
+#include <set>
+
+
+namespace datastructures {
+
+
+
+    class Counts{
+    public:
+        Counts(){
+            counts_=0;
+        }
+        Counts(int x): counts_(x){}
+
+        Counts &operator++(){
+            counts_+=1;
+            return *this;
+        }
+        operator int(){return counts_;}
+
+        friend bool operator<(const Counts &x, const Counts &y) {
+            return x.counts_ < y.counts_;
+        }
+
+        friend bool operator>(const Counts &x, const Counts &y) {
+            return x.counts_ > y.counts_;
+        }
+
+        friend bool operator==(const Counts &x, const Counts &y) {
+            return x.counts_ == y.counts_;
+        }
+
+
+    private:
+        int counts_;
+
+    };
+
+    class Word{
+    public:
+        Word():word_() {}
+        Word(std::string word): word_(word){}
+
+        friend bool operator<(const Word &x, const Word &y) {
+            return x.word_ < y.word_;
+        }
+        friend bool operator==(const Word &x, const Word &y) {
+
+            return x.word_ == y.word_;
+        }
+
+
+
+    private:
+        std::string word_;
+
+    };
+
+    class WordCounter {
+    public:
+
+
+        static Counts not_found; //for operator[] , because it need reference
+        WordCounter():index_(){};
+        WordCounter (const std::initializer_list<Word> &elements);
+
+        static WordCounter FromInputStream (std::istream &text);
+        int DistinctWords ()const;
+        int TotalWords()const;
+        std::set<Word> Words()const;
+
+        // []
+        Counts& operator[](std::string word);
+
+
+
+        // <<
+
+        friend std::ostream &operator<< (std::ostream &output, const WordCounter &counter);
+        //posortowane malejąco
+
+
+    private:
+        //kontener z <Word,Counts>
+        std::map <Word,Counts> index_;
+
+
+
+
+    };
+}
+
+
+
 
 #endif //JIMP_EXERCISES_WORDCOUNTER_H
